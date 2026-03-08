63e17ef8f8e3bb15fdd3070a2c599ef55757c18e
henrietteumugwaneza <umugwanezahenriette13@gmail.com>
2026-03-08 12:08:52 +0200

    search bar components

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+function SearchBar({ search, setSearch }) {
+
+return (
+
+<input
+type="text"
+placeholder="Search by name or email..."
+value={search}
+onChange={(e)=>setSearch(e.target.value)}
+/>
+
+)
+
+}
+
+export default SearchBar
